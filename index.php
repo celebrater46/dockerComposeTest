9c8a0d700e2316390abf642cec566f3c984e7402
hideru <jota.16.stigma.world@gmail.com>
2021-06-29 02:39:19 +0900

    All directory and files have been prepared

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <title>テスト</title>
 <?php
 try {
+//    https://qiita.com/fujitak/items/b56122e2ecd94022a7b6
     # hostには「docker-compose.yml」で指定したコンテナ名を記載
     $dsn = "mysql:host=mysql;dbname=sample;";
     $db = new PDO($dsn, 'root', 'pass');
